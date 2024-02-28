02477c64833133053219d408402b2990ad1b23e0
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-02-28 17:18:10 -0500

    Changed to macroquad instead of egui

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,45 @@
-use eframe::{egui, epaint::Color32};
+use macroquad::prelude::*;
 
-fn main() -> Result<(), eframe::Error> {
-    eframe::run_native(
-        DeadPixelCheck::APP_NAME,
-        DeadPixelCheck::native_options(),
-        DeadPixelCheck::app_creator(),
-    )?;
-    return Ok(());
-}
-
-pub struct DeadPixelCheck {
-    color: Color32,
-}
-impl DeadPixelCheck {
-    pub const APP_NAME: &'static str = "Dead Pixel Check";
-    pub fn native_options() -> eframe::NativeOptions {
-        return eframe::NativeOptions {
-            viewport: egui::ViewportBuilder::default()
-                .with_fullscreen(true)
-                .with_resizable(false)
-                .with_active(true),
-            ..Default::default()
-        };
-    }
-    pub fn app_creator() -> eframe::AppCreator {
-        return Box::new(|_cc| {
-            Box::new(Self {
-                color: Color32::WHITE,
-            })
-        });
-    }
-    fn change_color(&mut self) {
-        self.color = match self.color {
-            Color32::WHITE => Color32::BLACK,
-            Color32::BLACK => Color32::RED,
-            Color32::RED => Color32::GREEN,
-            Color32::GREEN => Color32::BLUE,
-            _ => Color32::WHITE,
-        };
+fn configuration() -> Conf {
+    Conf {
+        window_title: String::from("Dead pixel check"),
+        fullscreen: true,
+        ..Default::default()
     }
 }
-impl eframe::App for DeadPixelCheck {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        use egui::{CentralPanel, Key, Frame};
 
-        if ctx.input(|i| i.key_pressed(Key::Escape)) {
-            std::process::exit(0);
+fn any_input() -> bool {
+    let any_key_pressed = get_keys_pressed().len() > 0;
+    let any_mouse_button_pressed = is_mouse_button_pressed(MouseButton::Left) || is_mouse_button_pressed(MouseButton::Middle) || is_mouse_button_pressed(MouseButton::Right);
+    let any_touches = touches().len() > 0;
+    return any_key_pressed || any_mouse_button_pressed || any_touches;
+}
+
+fn cycle_color(color: &mut Color) {
+    *color = if *color == WHITE {
+        BLACK
+    } else if *color == BLACK {
+        RED
+    } else if *color == RED {
+        GREEN
+    } else if *color == GREEN {
+        BLUE
+    } else {
+        WHITE
+    };
+}
+
+#[macroquad::main(configuration)]
+async fn main() {
+    let mut color = WHITE;
+
+    loop {
+        clear_background(color);
+
+        if any_input() {
+            cycle_color(&mut color);
         }
 
-        if ctx.input(|i| i.key_pressed(Key::Space) || i.pointer.any_click() || i.any_touches()) {
-            self.change_color();
-        }
-
-        CentralPanel::default()
-            .frame(Frame {
-                fill: self.color,
-                ..Default::default()
-            })
-            .show(ctx, |_| {});
+        next_frame().await;
     }
 }
